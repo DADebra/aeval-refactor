422a79e8f62ce511751fab92f2b4b490ad8e301f
Daniel Riley <dmr05d@gmail.com>
2020-07-23 17:14:30 -0400

    supported arbitrary-precision arithmetic (+ optimizations)
    Co-authored-by: grigoryfedyukovich <grigory.fedyukovich@gmail.com>

--- include/ae/AeValSolver.hpp
+++ include/ae/AeValSolver.hpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 using namespace boost;
+using namespace expr::op::bind;
 namespace ufo
 {
 
@@ -1186,7 +1187,8 @@ namespace ufo
     SMTUtils u(efac);
     if (vars.size() == 0) return simplifyBool(cond);
 
-    Expr newCond = simpleQE(cond, vars);
+    Expr newCond = simplifyArithm(simpleQE(cond, vars));
+
     if (!emptyIntersect(newCond, vars) &&
         !containsOp<FORALL>(cond) && !containsOp<EXISTS>(cond) && !isNonlinear(newCond))
     {
@@ -1200,6 +1202,8 @@ namespace ufo
 
     ExprSet cnj;
     getConj(newCond, cnj);
+    ineqMerger(cnj, true);
+
     for (auto it = cnj.begin(); it != cnj.end(); )
     {
       ExprVector av;
