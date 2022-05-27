DADebra/aeval

Manually Print NumericOps
Previously, we were relying on Z3 to do the printing for e.g. +.
However, if Z3 would have output the nasty 'let' expressions in any of the
operator's arguments, this isn't prevented. By printing these operators
ourselves, we avoid this.

include/ae/SMTUtils.hpp
@@ -691,6 +691,7 @@ namespace ufo
         if (isOpX<LT>(e)) os << "(< ";
         if (isOpX<GT>(e)) os << "(> ";
         if (isOpX<NEQ>(e)) os << "(distinct ";
+        assert(e->arity() == 2);
         print(e->left(), os);
         os << " ";
         print(e->right(), os);
@@ -704,6 +705,24 @@ namespace ufo
         print(e->right(), os);
         os << " ";
         print(e->last(), os);
+        os << ")";
+      }
+      else if (isOp<NumericOp>(e))
+      {
+        if (isOpX<PLUS>(e)) os << "(+ ";
+        if (isOpX<MINUS>(e)) os << "(- ";
+        if (isOpX<MULT>(e)) os << "(* ";
+        if (isOpX<DIV>(e)) os << "(/ ";
+        if (isOpX<IDIV>(e)) os << "(/ ";
+        if (isOpX<MOD>(e)) os << "(mod ";
+        if (isOpX<REM>(e)) os << "(mod ";
+        if (isOpX<UN_MINUS>(e)) os << "(- ";
+        if (isOpX<ABS>(e)) os << "(abs ";
+        for (int i = 0; i < e->arity(); ++i)
+        {
+          print(e->arg(i), os);
+          if (i != e->arity() - 1) os << " ";
+        }
         os << ")";
       }
       else os << z3.toSmtLib (e);

DADebra/aeval-refactor

include/expr/SMTUtils.hpp
@@ -706,6 +706,7 @@ namespace ufo
         if (isOpX<LT>(e)) out << "(< ";
         if (isOpX<GT>(e)) out << "(> ";
         if (isOpX<NEQ>(e)) out << "(distinct ";
+        assert(e->arity() == 2);
         print(e->left(), out);
         out << " ";
         print(e->right(), out);
@@ -719,6 +720,24 @@ namespace ufo
         print(e->right(), out);
         out << " ";
         print(e->last(), out);
+        out << ")";
+      }
+      else if (isOp<NumericOp>(e))
+      {
+        if (isOpX<PLUS>(e)) out << "(+ ";
+        if (isOpX<MINUS>(e)) out << "(- ";
+        if (isOpX<MULT>(e)) out << "(* ";
+        if (isOpX<DIV>(e)) out << "(/ ";
+        if (isOpX<IDIV>(e)) out << "(/ ";
+        if (isOpX<MOD>(e)) out << "(mod ";
+        if (isOpX<REM>(e)) out << "(mod ";
+        if (isOpX<UN_MINUS>(e)) out << "(- ";
+        if (isOpX<ABS>(e)) out << "(abs ";
+        for (int i = 0; i < e->arity(); ++i)
+        {
+          print(e->arg(i), out);
+          if (i != e->arity() - 1) out << " ";
+        }
         out << ")";
       }
       else out << z3.toSmtLib (e);